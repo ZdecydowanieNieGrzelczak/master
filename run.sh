7291c4fd776745f2acc9a8a2f3ac0c694caf4747
Maciek <mgrzelczak97@gmail.com>
2022-07-02 16:20:25 +0200

    Change to 4k simplified

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J standard_4k_true
+#BSUB -J simplified_4k
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 4
 ### -- specify that the cores must be on the same host -- 
@@ -31,4 +31,4 @@
 
 
 # here follow the commands you want to execute 
-rel/master > rel/texts/new/true_standard_4k.txt
+rel/master > rel/texts/new/simplified_4k.txt

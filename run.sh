5e263b409d9566aa1c3f270a0b676e9bbb658440
Maciek Grzelczak <mgrzelczak97@gmail.com>
2022-07-18 00:14:59 +0200

    Changed to stan

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J inverse-relu-simp
+#BSUB -J inverse-relu-stan
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 4
 ### -- specify that the cores must be on the same host -- 
@@ -31,4 +31,4 @@
 
 
 # here follow the commands you want to execute 
-rel/master > rel/texts/fixed/inverse-relu-simp.txt
+rel/master > rel/texts/fixed/inverse-relu-stan.txt

d3a8c16c702043edb585ab3abffbe541a7da048b
Maciek Grzelczak <mgrzelczak97@gmail.com>
2022-07-18 00:12:43 +0200

    Inverse relu

--- NEAT/Neuron.cpp
+++ NEAT/Neuron.cpp
@@ -30,7 +30,7 @@ void Neuron::passValue() {
     for (const auto  out : outgoing) {
         if (out->isEnabled()) {
             double weight = out->getWeight();
-            auto val = std::max((currentValue + bias) * weight, 0.0);
+            auto val = std::min((currentValue + bias) * weight, 0.0);
             out->destination->receiveValue(val);
         }
     }
--- utils/SharedDefinitions.h
+++ utils/SharedDefinitions.h
@@ -41,7 +41,7 @@
 #define NOT_FOUND_CODE 404
 
 
-#define FILENAME_BASE "relu-simp-true"
+#define FILENAME_BASE "inverse-relu-simp"
 
 #define PRUNE_EVERY_N 100
 
